75452fb64895a1b3eeaa30dd30644bf3ff183c1c
Divya251199 <92331091+Divya251199@users.noreply.github.com>
2023-04-14 23:43:26 +0530

    Add files via upload

---
+++ SendMailJava/src/main/java/org/geekster/MailConstants.java
@@ -0,0 +1,8 @@
+package org.geekster;
+
+public class MailConstants {
+    public static final String SENDER = "divyahirve2511@gmail.com";
+    public static final String RECEIVERMAILADDRESS = "hirveparth09@gmail.com";
+    public static final String SUBJECT = "Testing Java mail : 12-April-2023";
+    public static final String MESSAGE = "This message is for security check...!!!";
+}
